2d7ee9031b7b9061bdca8bb334b74a5069178ad7
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-04-29 11:17:15 -0700

    post component

--- src/Pages/Post.jsx
+++ src/Pages/Post.jsx
@@ -1,6 +1,61 @@
+import { useState, useEffect } from 'react';
+
 const PostComponent = (props) => {
-    return <h1>Component Name:Comp</h1>
-  }
-  
-  export default PostComponent
-  
+    const [post, setPost] = useState([])
+
+    const BASE_URL = "http://localhost:4000/posts"
+
+    const getPost = async () => {
+        try {
+            const response = await fetch(BASE_URL)
+            const allPosts = await response.json()
+            setPost(allPosts)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        getPost()
+    }, [])
+
+    console.log(`There are ${post.length} posts available to render`)
+
+    const loaded = () => {
+        return post.map((el) => {
+            return (
+                <div key={post._id}>
+                    <p>{post.title}</p>
+                    <p>{post.description}</p>
+                    <img src={post.image} />
+                    <p>{post.price}</p>
+                </div>
+            );
+        });
+    };
+
+    const loading = () => (
+        <section>
+            <h1>
+                Loading...
+                <span>
+                    <img
+                        className="spinner"
+                        src="https://freesvg.org/img/1544764567.png"
+                    />{" "}
+                </span>
+            </h1>
+        </section>
+    );
+
+
+
+
+    return (
+        <section className="people-list">
+            {post && post.length ? loaded() : loading()}
+        </section>
+    )
+}
+
+export default PostComponent
