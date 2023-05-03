48eb9264c2f2ef4e2deb6ab44447caf66a6ed4eb
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-05-02 22:59:18 -0700

    register user function input in auth

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -1,7 +1,61 @@
-function Auth(props) {
+import userEvent from '@testing-library/user-event';
+import { useContext } from 'react';
+import { UserContext } from '../Data'
+import { setUserToken, clearUserToken } from '../utils/authToken'
+import RegisterForm from '../Components/RegisterForm';
+
+
+function Auth() {
+    const { setAuth, setUser } = useContext(UserContext)
+    // console.log("Testing Context", setAuth, setUser)
+    const registerUser = async (data) => {
+        try {
+
+            const configs = {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+
+            const newUser = await fetch(
+                "http://localhost:4000/auth/register",
+                configs
+            )
+            console.log(newUser)
+
+            const parsedUser = await newUser.json()
+
+            if (parsedUser.token) {
+                // sets local storage
+                setUserToken(parsedUser.token)
+                // put the returned user object in state
+                setUser(parsedUser.user)
+                // adds a boolean cast of the responses isAuthenticated prop
+                setAuth(parsedUser.isLoggedIn)
+
+                // alternative (safer) implementation would be to use jwt decode library - <https://www.npmjs.com/package/jwt-decode>
+                // this would also require reconfiguring our backend so we only send tokens with a signup
+
+                return parsedUser
+            } else {
+                throw `Server Error: ${parsedUser.err}`
+            }
+
+        } catch (err) {
+            console.log(err)
+            clearUserToken();
+            setAuth(false);
+            return null
+        }
+    }
+
     return (
-        <h1>Login / Register Container</h1>
+        <section className='container'>
+            <RegisterForm signUp={registerUser} />
+        </section>
     )
 }
 
-export default Auth
+export default Auth;
