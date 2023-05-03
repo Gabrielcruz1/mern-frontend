bb0fe3595a69880a92d8ccf3ac9aebd3de58a925
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-05-02 22:23:58 -0700

    added context values to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { UserContext } from "./Data";
+import { useState, useEffect, useContext } from 'react'
 import Main from "./Components/Main";
 import Header from "./Components/Header";
 
 function App() {
+
+  const { Provider: UserInfo } = UserContext
+  // this allows us to rename the React Context Provider to something more descriptive
+  // example: UserInfo
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [currentUser, setCurrentUser] = useState(null)
+
   return (
-    <>
-      < Header />
-      < Main />
-    </>
+    <div className='App'>
+      <UserInfo value={{
+        isAuthenticated,
+        setAuth: setIsAuthenticated,
+        user: currentUser,
+        setUser:
+          setCurrentUser
+
+      }}
+      >
+        < Header />
+        < Main />
+      </UserInfo>
+
+    </div>
   );
 }
 
